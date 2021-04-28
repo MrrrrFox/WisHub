73ca19380814ab00cf6e837147f49b837315445d
Tymek <tymoteusz@akcja.pl>
2021-04-28 10:52:24 +0200

    Adding some fancier views for the admin panel

--- server/io_project/wishub/admin.py
+++ server/io_project/wishub/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import Post, Subject, Domain
 
+#Adding more fancy Admin views
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('description', 'link', 'author',
+                    'subject', 'level', 'created',
+                    'num_upvoted', 'num_downvoted')
+    list_filter = ('created', 'num_upvoted', 'level', 'author')
+    date_hierarchy = 'created'
+    ordering = ['-num_upvoted', '-created']
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'domain')
+    list_filter = ('domain',)
+
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Domain)
-admin.site.register(Subject)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Domain, DomainAdmin)
+admin.site.register(Subject, SubjectAdmin)
