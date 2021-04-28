029d2885990398ff4122aea252a2e1a2bbbfd859
Tymek <tymoteusz@akcja.pl>
2021-04-28 23:49:32 +0200

    Adding the custom inner function, to list posts by their subject

--- server/io_project/wishub/views.py
+++ server/io_project/wishub/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
 from .models import Post, Subject, Domain
 from .serializers import PostSerializer, SubjectSerializer, DomainSerializer
 from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import viewsets
 
 # Create your views here.
 
 #After tutorial:
 class PostViewSet(viewsets.ModelViewSet):
+    '''ViewSet class for the Posts
+
+    Custom function to filter all the posts from the desired subject.
+    To get all the posts from the subject e.g. with id=1, use URL like below:
+    /posts/1/by-domain/'''
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    @action(methods=['get'], detail=True, url_path='by-domain',
+        url_name='by_domain')
+    def get_by_subject_id(self, request, pk=None):
+        queryset = Post.objects.filter(subject = pk)
+        serializer = PostSerializer(queryset, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
 class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Domain.objects.all()
