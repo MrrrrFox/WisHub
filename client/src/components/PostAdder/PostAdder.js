ec5ac8c95bc37fe0f38a3cda7134faba81ba9159
Marta <marta1317.mg@gmail.com>
2021-05-02 15:37:03 +0200

    PostAdder form

--- client/src/components/PostAdder/PostAdder.js
+++ client/src/components/PostAdder/PostAdder.js
@@ -1,14 +1,210 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect} from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { useHistory } from 'react-router-dom';
+import { useForm, Controller, FormProvider } from "react-hook-form";
+import { FormControl, TextField, MenuItem } from "material-ui/core";
+import axios from "../../axios.config";
 
 
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        WisHub
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const Subjects = [
+  { value: "mathematics", text: "Mathematics" },
+  { value: "physics", text: "Physics" },
+  { value: "programming", text: "Programming" },
+  { value: "computer science", text: "Computer Science" },
+];
+
+const Levels = [
+  { value: "BE", text: "Beginer" },
+  { value: "IN", text: "Intermediate" },
+  { value: "AD", text: "Advanced" },
+];
+
+  const { handleSubmit, control } = useForm();
+  const classes = useStyles();
+  const history = useHistory();
 
 
-const PostAdder = () => {
+  const handlePostAdder = (user) => {
+
+
+    axios.post('v1/users/auth/post-add/',user)
+      .then(res => {
+        if(res.status === 201){
+          history.push("/posts/:id")
+        }
+      })
+      .catch((error) => {
+        if( error.response ){
+          console.log(error.response.data); // => the response payload
+          }
+      });
+
+  };
 
   return (
-      'Form for adding post'
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <FormProvider { ...handleSubmit }>
+        <form
+          onSubmit = {handleSubmit(handlePostAdder)}
+          className={classes.form}
+        >          <Controller
+            render = {({field})=> (
+                <TextField {...field}
+                    fullWidth
+                    label="Author"
+                    required
+                    onChange={(e) => field.onChange(e)}
+                    value={field.value}
+                />
+            )}
+            name="author"
+            control={control}
+            defaultValue=""
+            label="Author"
+          />
+
+          <Controller
+            render = {({field})=> (
+                <CharField {...field}
+                    fullWidth
+                    label="Link"
+                    required
+                    onChange={(e) => field.onChange(e)}
+                    value={field.value}
+                    inputProps={{ maxLength: 100 }}
+                />
+            )}
+            name="link"
+            control={control}
+            defaultValue=""
+            label="Link"
+          />
+
+          <Controller
+            render = {({field})=> (
+                <TextField {...field}
+                           fullWidth
+                           label="Description"
+                           required
+                           onChange={(e) => field.onChange(e)}
+                           value={field.value}
+                           inputProps={{ maxLength: 300 }}
+                />
+            )}
+            name="description"
+            control={control}
+            defaultValue=""
+            />
+
+          <FormControl>
+              <TextField
+                select
+                label="Choose one subject"
+                id="subject"
+                inputProps={{
+                  inputRef: (ref) => {
+                    if (!ref) return;
+                    Subjects({
+                      name: "Subjects",
+                      value: ref.value,
+                    });
+                  },
+                }}
+              >
+              {Subjects.map((subject) => (
+                    <MenuItem key={subject.value} value={subject.value}>
+                          {subject.text}
+                    </MenuItem>
+                ))}
+              </TextField>
+          </FormControl>
+
+          <FormControl>
+              <TextField
+                select
+                label="Choose the level"
+                id="lvl"
+                inputProps={{
+                  inputRef: (ref) => {
+                    if (!ref) return;
+                    Level({
+                      name: "Level",
+                      value: ref.value,
+                    });
+                  },
+                }}
+              >
+              {Levels.map((level) => (
+                    <MenuItem key={level.value} value={level.value}>
+                          {level.text}
+                    </MenuItem>
+                ))}
+              </TextField>
+          </FormControl>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Create
+          </Button>
+        </form>
+        </FormProvider>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
   );
-};
 
 
 
